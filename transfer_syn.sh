c89e615deefe78cbab84d4f4a748c514969e002b
Your Name <ubuntu@Thinkpad.localdomain>
2026-05-23 13:20:56 +0100

    files

--- transfer_syn.sh
+++ transfer_syn.sh
@@ -6,4 +6,4 @@ python3 ../configure.py
 ambuild
 
 cp -v package/addons/sourcemod/extensions/server_utils.ext.2.sdk2013.so ../manual_build
-scp /home/ubuntu/alliedmodders/sourcemod/public/SourceEngineReverseEngineering/manual_build/server_utils.ext.2.sdk2013.so tom@gameserverhost:/home/tom/.local/share/Steam/steamapps/common/Synergy/synergy/addons/sourcemod/extensions
+#scp /home/ubuntu/alliedmodders/sourcemod/public/SourceEngineReverseEngineering/manual_build/server_utils.ext.2.sdk2013.so tom@gameserverhost:/home/tom/.local/share/Steam/steamapps/common/Synergy/synergy/addons/sourcemod/extensions
