ed8d3587d79e0b797a8f37b3e393a114e5a4ba52
Your Name <ubuntu@Thinkpad.localdomain>
2026-06-21 10:15:59 +0100

    FILES

--- transfer_syn.sh
+++ transfer_syn.sh
@@ -7,3 +7,6 @@ ambuild
 
 cp -v package/addons/sourcemod/extensions/server_utils.ext.2.sdk2013.so ../manual_build
 #scp /home/ubuntu/alliedmodders/sourcemod/public/SourceEngineReverseEngineering/manual_build/server_utils.ext.2.sdk2013.so tom@gameserverhost:/home/tom/.local/share/Steam/steamapps/common/Synergy/synergy/addons/sourcemod/extensions
+
+git add --all
+git commit -m "FILES"
